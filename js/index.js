a38369e5207ce9dc7b7da62fdc31305188f72a0e
Omar Zohdi <omarzohdi@gmail.com>
2015-10-14 06:50:20 -0500

    CSS and Projects update
    Added banners, url link and some text to projects fixed some minor bugs
    and fixed CSS for mobile devices.

--- js/index.js
+++ js/index.js
@@ -1,14 +1,105 @@
 
-$(document).ready(function() {
-        $("body").css("display", "none");
-        $("body").fadeIn(2000);
+$(document).ready(function() 
+{
+    $("body").css("display", "none");
+    $("body").fadeIn(2000);
+    $(".Projects").css("display", "none");
+    $(".Navbarbtm").css("display","none");
+
+	$("#id_projects").on("click", OpenProjectsPage);
+	$("#id_awards").on("click", OpenAwardPage);
+	$("#id_back").on("click", OpenHomePage);
+	$('#id_top').on("click", BacktoTop);
+
+	var ishome = true;
+	var cimgw;
+	
+	function ClickAnimationCheck(isAnimating)
+	{
+		if (isAnimating)
+		{
+			$("#id_projects").off("click", OpenProjectsPage);
+			$("#id_back").off("click", OpenHomePage);
+			$('#id_top').off("click", BacktoTop);
+			$("#id_awards").off("click", OpenAwardPage);
+		}
+		else
+		{
+			$("#id_projects").on("click", OpenProjectsPage);
+			$("#id_back").on("click", OpenHomePage);			
+			$('#id_top').on("click", BacktoTop);
+			$("#id_awards").on("click", OpenAwardPage);
+		}
+	}
+	
+	function BacktoTop()
+	{
+		$('body').animate({
+          scrollTop: 0
+        }, 1000);
+	}
+
+	function OpenAwardPage()
+	{
+	//	ClickAnimationCheck(true);
+
+	}
+
+	function OpenHomePage()
+	{
+		ClickAnimationCheck(true);
+		var cimg = $(".CoverImage");
+
+		cimg.removeClass("translatecoverimage");
+		$(".Navbarbtm").fadeToggle(1000, function(){
+			$(".Social").fadeToggle(1000);	
+		});
+
+		$(".Projects").fadeToggle(1000, function(){
+			$(".Description").fadeToggle(1000);
+			cimg.animate({width: cimgw, marginTop: '0px'}, 1000 );
+			ClickAnimationCheck(false);
+		});
+
+		ishome = true;	
+	}
+
+	function OpenProjectsPage ()
+	{
+		if (ishome)
+		{
+			ClickAnimationCheck(true);
+			var cimg = $(".CoverImage");
+			cimgw = $(".CoverImage").width();
+
+			cimg.animate({ width: '100px', marginTop: '-40px'}, 1000);
+			cimg.addClass("translatecoverimage");	
+
+			$(".Social").fadeToggle(1000, function(){
+				$(".Navbarbtm").fadeToggle(1000);	
+			});
+
+			$(".Description").fadeToggle(1000, function() {
+				$(".Projects").fadeToggle(1000);
+				ClickAnimationCheck(false);
+			});
+
+			ishome = false;
+		}
+		else
+		{
+			OpenHomePage();
+		}
+	}
 });
 
-if (screen.width <= 699) {
+if (screen.width <= 699) 
+{
   changeCSS("css/mobile.css", 0);
 }
 
-function changeCSS(cssFile, cssLinkIndex) {
+function changeCSS(cssFile, cssLinkIndex) 
+{
   var oldlink = document.getElementsByTagName("link").item(cssLinkIndex);
   var newlink = document.createElement("link");
   newlink.setAttribute("rel", "stylesheet");
@@ -17,3 +108,4 @@ function changeCSS(cssFile, cssLinkIndex) {
 
   document.getElementsByTagName("head").item(0).replaceChild(newlink, oldlink);
 }
+
