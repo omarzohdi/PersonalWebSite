0e44ac6ec7d1916f90b66b2e8b782f56a66d3457
Omar Zohdi <omarzohdi@gmail.com>
2015-10-18 09:35:43 -0500

    Mobile Css
    Moved Mobile css to main css using media queries and updated js

--- js/index.js
+++ js/index.js
@@ -85,19 +85,3 @@ $(document).ready(function()
       currpage = nextpage;
   }
 });
-
-if (screen.width <= 699)
-{
-  changeCSS("css/mobile.css", 0);
-}
-
-function changeCSS(cssFile, cssLinkIndex)
-{
-  var oldlink = document.getElementsByTagName("link").item(cssLinkIndex);
-  var newlink = document.createElement("link");
-  newlink.setAttribute("rel", "stylesheet");
-  newlink.setAttribute("type", "text/css");
-  newlink.setAttribute("href", cssFile);
-
-  document.getElementsByTagName("head").item(0).replaceChild(newlink, oldlink);
-}
